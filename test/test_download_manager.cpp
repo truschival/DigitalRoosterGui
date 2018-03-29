8e8e4abfbb1d375023a2c311775ae3d53147da12
Thomas Ruschival <thomas@ruschival.de>
2018-03-29 11:22:28 +0200

    Cleanup tests

--- test/test_download_manager.cpp
+++ test/test_download_manager.cpp
@@ -1,12 +1,15 @@
-/*************************************************************************************
+/******************************************************************************
  * \filename
  * \brief
  *
  * \details
  *
- * \author ruschi
+ * \copyright (c) 2018  Thomas Ruschival <thomas@ruschival.de>
+ * \license {This file is licensed under GNU PUBLIC LICENSE Version 2 or later
+ * 			 SPDX-License-Identifier: GPL-2.0-or-later}
  *
- *************************************************************************************/
+ *****************************************************************************/
+
 #include <DownloadManager.hpp>
 
 namespace DigitalRooster {
