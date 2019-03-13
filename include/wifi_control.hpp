07f6cef434146fad08376d646a79799493dd9b2b
Thomas Ruschival <thomas@ruschival.de>
2019-03-13 20:01:44 +0100

    Add QT wrapper WifiControl
    - add compile time config for wpa_supplicant control socket

---
+++ include/wifi_control.hpp
@@ -0,0 +1,151 @@
+/******************************************************************************
+ * \filename
+ * \brief	Interface QT to wpa_supplicant
+ *
+ * \details
+ *
+ * \copyright (c) 2018  Thomas Ruschival <thomas@ruschival.de>
+ * \license {This file is licensed under GNU PUBLIC LICENSE Version 3 or later
+ * 			 SPDX-License-Identifier: GPL-3.0-or-later}
+ *
+ *****************************************************************************/
+
+#ifndef _WIFI_CONTROL_HPP_
+#define _WIFI_CONTROL_HPP_
+
+#include <QObject>
+#include <QString>
+#include <QStringRef>
+#include <QVector>
+#include <memory>
+
+
+#include "wpa_ctrl/wpa_ctrl.h"
+
+
+namespace DigitalRooster {
+// Forward declaration
+class ConfigurationManager;
+
+/**
+ * Function to register as callback
+ * @param buf
+ * @param len
+ */
+void wpa_msg_cb(char* buf, size_t len);
+
+class WifiNetwork;
+/**
+ * Abstracts access to wpa_ctrl code
+ * I can't believe I would use a singleton but I finally found a use case!
+ * We need to register a C-Function with the wpa_ctrl.c for notification
+ * Member functions are not allowed and I dont want multiple objects
+ * accessing the wpa_cli at once - Singleton to the help!
+ */
+class WifiControl : public QObject {
+    Q_OBJECT;
+    friend void DigitalRooster::wpa_msg_cb(char* buf, size_t len);
+
+public:
+    /**
+     * Singleton Creator method
+     * @param cm configuration manager (to get WIFI interface)
+     * @return
+     */
+    static WifiControl& get_instance(ConfigurationManager* cm = nullptr);
+    /**
+     * Trigger Push Button Configuration (PBC) authentication
+     * with given network
+     * @param network
+     */
+    Q_INVOKABLE void wps_pbc_auth(WifiNetwork& network);
+
+    /**
+     * Read the last scan result
+     * @return list of networks found
+     */
+    const QVector<WifiNetwork>& get_scan_result();
+
+    /**
+     * Destructor must close wpa_cli
+     */
+    virtual ~WifiControl();
+
+    /**
+     * Connect to wpa control interface socket
+     */
+    void connect_wpa_control_socket();
+
+public slots:
+    void start_scan();
+
+signals:
+    /**
+     * Scan was successful and result is available
+     */
+    void networks_found(const QVector<WifiNetwork>& network);
+    /**
+     * Scan was successful and result is available
+     */
+    void scan_finished();
+
+private:
+    /**
+     * Construct with path to event interface for rotary encoder and
+     * push button GPIO
+     */
+    WifiControl(QObject* parent = nullptr);
+
+    /**
+     * Holds last scan result
+     */
+    QVector<WifiNetwork> scan_results;
+    /**
+     * Handle for lower layer wpa_ctrl
+     */
+    struct wpa_ctrl* ctrl;
+
+    /**
+     * Path to management socket e.g. /var/lib/wpa_supplicant/wlan0
+     */
+    QString wpa_supplicant_sock_path;
+
+    /**
+     * Parse wpa layer notifications
+     * @param buffer notification string
+     * @param len size of buffer
+     */
+    void wpa_notification(char* buffer, size_t len);
+};
+
+
+/**
+ * Tokenize a single line of scan results and interpret fields as WifiNetwork
+ * @param line refernce to string in scan_results
+ * @return WifiNetwork data object
+ */
+WifiNetwork line_to_network(const QStringRef& line);
+
+/**
+ * Tokenize a buffer returend by wpa_ctrl SCAN_RESULT into a vector of
+ * WifiNetworks
+ * @param buffer result
+ * @param len buffer length
+ */
+QVector<WifiNetwork> parse_scanresult(char* buffer, size_t len);
+
+
+/**
+ * POD class for Wifi network information
+ */
+class WifiNetwork {
+public:
+    QString bssid;
+    QString name;
+    int signal_strength;
+    bool wps_available;
+};
+
+} // namespace DigitalRooster
+
+#endif /* _WIFI_CONTROL_HPP_ */
---
+++ libsrc/wifi_control.cpp
@@ -0,0 +1,120 @@
+/******************************************************************************
+ * \filename
+ * \brief	Interface QT to wpa_supplicant
+ *
+ * \details
+ *
+ * \copyright (c) 2018  Thomas Ruschival <thomas@ruschival.de>
+ * \license {This file is licensed under GNU PUBLIC LICENSE Version 3 or later
+ * 			 SPDX-License-Identifier: GPL-3.0-or-later}
+ *
+ *****************************************************************************/
+
+#include <QLoggingCategory>
+#include <QString>
+
+#include "appconstants.hpp"
+#include "configuration_manager.hpp"
+#include "wifi_control.hpp"
+#include "wpa_ctrl/wpa_ctrl.h"
+
+using namespace DigitalRooster;
+
+static Q_LOGGING_CATEGORY(CLASS_LC, "DigitalRooster.WifiControl");
+
+
+/****************************************************************************/
+
+/**
+ * C Callback function to receive events from wpa_ctrl.c
+ * @param buf char buffer, allocated by user
+ * @param len buffer length
+ */
+void DigitalRooster::wpa_msg_cb(char* buf, size_t len) {
+    qCDebug(CLASS_LC) << Q_FUNC_INFO << buf;
+    WifiControl::get_instance().wpa_notification(buf, len);
+}
+
+
+/****************************************************************************/
+WifiControl& WifiControl::get_instance(ConfigurationManager* cm) {
+    qCDebug(CLASS_LC) << Q_FUNC_INFO;
+    static WifiControl instance;
+    if (cm) {
+        instance.wpa_supplicant_sock_path = cm->get_wpa_socket_name();
+        instance.connect_wpa_control_socket();
+    }
+    return instance;
+}
+/****************************************************************************/
+WifiControl::WifiControl(QObject* parent)
+    : QObject(parent)
+    , ctrl(nullptr) {
+    qCDebug(CLASS_LC) << Q_FUNC_INFO;
+}
+
+/****************************************************************************/
+void WifiControl::connect_wpa_control_socket() {
+    qCDebug(CLASS_LC) << Q_FUNC_INFO << wpa_supplicant_sock_path;
+    char buf[128];
+    ctrl = wpa_ctrl_open(wpa_supplicant_sock_path.toStdString().c_str());
+    if (!ctrl) {
+        qCCritical(CLASS_LC) << " opening wpa_ctrl failed!";
+    } else {
+        // send ping
+        size_t buf_len = sizeof(buf);
+        wpa_ctrl_request(
+            ctrl, "PING", sizeof("PING"), buf, &buf_len, wpa_msg_cb);
+        qCDebug(CLASS_LC) << buf;
+    }
+}
+/****************************************************************************/
+WifiControl::~WifiControl() {
+    qCDebug(CLASS_LC) << Q_FUNC_INFO;
+    if (ctrl) {
+        wpa_ctrl_close(ctrl);
+    }
+}
+/****************************************************************************/
+void WifiControl::wps_pbc_auth(WifiNetwork& network) {
+    qCDebug(CLASS_LC) << Q_FUNC_INFO;
+}
+
+/****************************************************************************/
+const QVector<WifiNetwork>& WifiControl::get_scan_result() {
+    qCDebug(CLASS_LC) << Q_FUNC_INFO;
+    return scan_results;
+}
+
+/****************************************************************************/
+void WifiControl::wpa_notification(char* buffer, size_t len) {
+    qCDebug(CLASS_LC) << Q_FUNC_INFO;
+    qCDebug(CLASS_LC) << buffer;
+}
+
+/****************************************************************************/
+void WifiControl::start_scan() {
+    qCDebug(CLASS_LC) << Q_FUNC_INFO;
+}
+
+/****************************************************************************/
+WifiNetwork DigitalRooster::line_to_network(const QStringRef& line) {
+    QVector<QStringRef> list = line.split("\t");
+    WifiNetwork nw{list[4].toString(), list[0].toString()};
+    return nw;
+}
+
+/****************************************************************************/
+QVector<WifiNetwork> DigitalRooster::parse_scanresult(
+    char* buffer, size_t len) {
+    QString results(buffer);
+    auto lines = results.splitRef("\n");
+    QVector<WifiNetwork> cont;
+    // skip first line which is header of table
+    for (int i = 1; i < lines.length(); i++) {
+        cont.push_back(line_to_network(lines[i]));
+    }
+    return cont;
+}
+
+/****************************************************************************/
