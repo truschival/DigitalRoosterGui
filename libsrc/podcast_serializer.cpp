494f8b0cf07279803331d2e4dec4962352b2a7b6
Thomas Ruschival <thomas@ruschival.de>
2019-06-24 17:51:51 +0200

    add cached icon to podcast_serializer

--- libsrc/podcast_serializer.cpp
+++ libsrc/podcast_serializer.cpp
@@ -106,7 +106,7 @@ void PodcastSerializer::parse_podcast_source_from_json(
         auto img_cached = tl_obj[KEY_IMAGE_CACHE].toString();
         ps->set_title(title);
         ps->set_description(desc);
-        ps->set_image_uri(img_url);
+        ps->set_image_url(img_url);
         ps->set_image_file_path(img_cached);
     }
     auto episodes_json_array = tl_obj[KEY_EPISODES].toArray();
@@ -163,7 +163,7 @@ QJsonObject DigitalRooster::PodcastSerializer::json_from_podcast_source(
     ps_obj[KEY_TIMESTAMP] = wallclock->now().toString();
     ps_obj[KEY_TITLE] = ps->get_title();
     ps_obj[KEY_DESCRIPTION] = ps->get_description();
-    ps_obj[KEY_ICON_URL] = ps->get_image_uri().toString();
+    ps_obj[KEY_ICON_URL] = ps->get_image_url().toString();
     ps_obj[KEY_IMAGE_CACHE] = ps->get_image_file_path();
     return ps_obj;
 }
