2fd43953d0ca8ad17c97ba67c2f931700589468e
Thomas Ruschival <thomas@ruschival.de>
2019-12-06 10:53:37 +0100

    Add REST component

---
+++ include/REST/rest_control.hpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * \filename
+ * \brief     REST adapter for configuration
+ *
+ * \details
+ *
+ * \copyright (c) 2018  Thomas Ruschival <thomas@ruschival.de>
+ * \license {This file is licensed under GNU PUBLIC LICENSE Version 3 or later
+ * 			 SPDX-License-Identifier: GPL-3.0-or-later}
+ *
+ *****************************************************************************/
+
+#ifndef INCLUDE_REST_REST_CONTROL_HPP_
+#define INCLUDE_REST_REST_CONTROL_HPP_
+
+#include <pistache/endpoint.h>
+
+void init();
+
+
+#endif /* INCLUDE_REST_REST_CONTROL_HPP_ */
