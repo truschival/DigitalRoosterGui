754c4db825340181f73a334120b1799f775e7a5b
Thomas Ruschival <thomas@ruschival.de>
2020-04-11 18:29:11 +0200

    API-Test alarm works with fixture
    read and create alarm works
    
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

--- test/api-tests/test_alarms.py
+++ test/api-tests/test_alarms.py
@@ -1,8 +1,17 @@
 
 import pytest
 import digitalrooster
-from digitalrooster import configuration
+from digitalrooster import configuration, api_client
 from digitalrooster.rest import ApiException
+
+
+def test_read_alarms(api_client):
+    c = digitalrooster.AlarmsApi(api_client)
+    r= c.alarms_read_all()
+    assert r[0].id == '{8364287c-3036-4cd0-b243-0a4f5dd863ae}'
+    assert r[0].period == 'workdays'
+    assert r[0].url == 'http://st01.dlf.de/dlf/01/128/mp3/stream.mp3'
+    assert r[0].enabled == True
 
 def test_create_alarm_ok(api_client):
      c = digitalrooster.AlarmsApi(api_client)
@@ -23,4 +32,4 @@ def test_create_alarm_wo_time_raises(api_client):
          c.alarms_create(alarm)   
      assert apiexc.value.status == 400
      assert str(apiexc.value.body) == "Alarm Time invalid!"
-     
+
