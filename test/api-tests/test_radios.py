4fe6fa6291b251400a18a2403125afecb3bc9178
Thomas Ruschival <thomas@ruschival.de>
2020-10-11 14:52:32 +0200

    Fix failing tests
    API-client tests should use correct API. Renamed Internet Radio to
    Radios...
    
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

--- test/api-tests/test_radios.py
+++ test/api-tests/test_radios.py
@@ -4,7 +4,7 @@ import json
 from digitalrooster.rest import ApiException
 
 def test_read_stations(api_client):
-    c = digitalrooster.InternetRadioApi(api_client)
+    c = digitalrooster.RadiosApi(api_client)
     r= c.iradio_read_all()
     assert len(r) == 2
     assert r[0].id == '0a2152ef-da4f-4f26-8a77-078193da536e'
@@ -13,12 +13,12 @@ def test_read_stations(api_client):
 
 
 def test_read_stations_negative_offset(api_client):
-    c = digitalrooster.InternetRadioApi(api_client)
+    c = digitalrooster.RadiosApi(api_client)
     r= c.iradio_read_all(offset=-1)
     assert len(r) == 2
 
 def test_get_station_by_id(api_client):
-    c = digitalrooster.InternetRadioApi(api_client)
+    c = digitalrooster.RadiosApi(api_client)
     r=c.iradio_read_one("09be8e85-a9d3-4db8-b2c5-02e3eb3ff66d")
     assert r.id == "09be8e85-a9d3-4db8-b2c5-02e3eb3ff66d"
     assert r.url == "https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3"
@@ -26,7 +26,7 @@ def test_get_station_by_id(api_client):
 
 
 def test_get_station_by_wrong_id(api_client):
-    c = digitalrooster.InternetRadioApi(api_client)
+    c = digitalrooster.RadiosApi(api_client)
     with pytest.raises(ApiException) as apiexc:
         c.iradio_read_one("c0ffee00-404c-f776-8619-3c4c2c4da212")
     assert apiexc.value.status == 400
@@ -36,7 +36,7 @@ def test_get_station_by_wrong_id(api_client):
 
 
 def test_create_station_ok(api_client):
-     c = digitalrooster.InternetRadioApi(api_client)
+     c = digitalrooster.RadiosApi(api_client)
      station = digitalrooster.Station(name="AName", url="foo://bar.baz")
      c.iradio_create(station)
      r= c.iradio_read_all()
@@ -44,7 +44,7 @@ def test_create_station_ok(api_client):
 
      
 def test_create_station_wrong_url(api_client):
-     c = digitalrooster.InternetRadioApi(api_client)
+     c = digitalrooster.RadiosApi(api_client)
      station = digitalrooster.Station(url="Not : # üß !an.url/",
                                  name="Thomas")
      with pytest.raises(ApiException) as apiexc:
@@ -53,13 +53,13 @@ def test_create_station_wrong_url(api_client):
      
     
 def test_delete_station(api_client):
-     c = digitalrooster.InternetRadioApi(api_client)
+     c = digitalrooster.RadiosApi(api_client)
      c.iradio_delete("09be8e85-a9d3-4db8-b2c5-02e3eb3ff66d")
      r= c.iradio_read_all()
      assert len(r) == 1
 
 def test_delete_station_wrong_id(api_client):
-     c = digitalrooster.InternetRadioApi(api_client)
+     c = digitalrooster.RadiosApi(api_client)
      with pytest.raises(ApiException) as apiexc:
          c.iradio_delete("c0ffee00-404c-f776-8619-3c4c2c4da212")
      assert apiexc.value.status == 400
