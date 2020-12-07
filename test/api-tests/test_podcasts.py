0572cced1579a1e7bd5573533983aa70a581cb70
Thomas Ruschival <thomas@ruschival.de>
2020-12-07 14:13:12 +0100

    Fix failing API tests
    Apparently behavior of the generator has changed.
    Now APIs and Models have their individual sub-package.
    
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

--- test/api-tests/test_podcasts.py
+++ test/api-tests/test_podcasts.py
@@ -7,10 +7,12 @@ import pytest
 import digitalrooster
 import json
 from digitalrooster.rest import ApiException
+from digitalrooster.api.podcasts_api import PodcastsApi
+from digitalrooster.model.podcast import Podcast
 
 def test_read_podcasts(api_client):
-    c = digitalrooster.PodcastsApi(api_client)
-    r= c.podcasts_read_all()
+    c = PodcastsApi(api_client)
+    r= c.podcasts_read_all().value
     assert len(r) == 2
     assert r[1].id == '6d2ebae6-d961-411d-aecc-7820d1be1650'
     assert r[1].title == 'Arms Control Wonk'
@@ -18,14 +20,14 @@ def test_read_podcasts(api_client):
     assert r[1].update_interval == 1200
 
 def test_get_podcast_by_id(api_client):
-    c = digitalrooster.PodcastsApi(api_client)
+    c = PodcastsApi(api_client)
     r=c.podcasts_read_one("61f6bcda-0334-49a7-bfe9-b206b9f0bef7")
     assert r.id == "61f6bcda-0334-49a7-bfe9-b206b9f0bef7"
     assert r.url == "https://alternativlos.org/alternativlos.rss"
     assert r.title == "Alternativlos"
 
 def test_get_podcast_by_wrong_id(api_client):
-    c = digitalrooster.PodcastsApi(api_client)
+    c = PodcastsApi(api_client)
     with pytest.raises(ApiException) as apiexc:
         c.podcasts_read_one("c0ffee00-404c-f776-8619-3c4c2c4da212")
     assert apiexc.value.status == 400
@@ -35,16 +37,16 @@ def test_get_podcast_by_wrong_id(api_client):
 
 
 def test_create_podcast_ok(api_client):
-     c = digitalrooster.PodcastsApi(api_client)
-     podcast = digitalrooster.Podcast(title="The Title", url="foo://bar.baz")
+     c = PodcastsApi(api_client)
+     podcast = Podcast(title="The Title", url="foo://bar.baz")
      x= c.podcasts_create(podcast)
-     r= c.podcasts_read_all()
+     r= c.podcasts_read_all().value
      assert len(r) == 3
 
      
 def test_create_podcast_wrong_url(api_client):
-     c = digitalrooster.PodcastsApi(api_client)
-     podcast = digitalrooster.Podcast(url="Not An URL!",
+     c = PodcastsApi(api_client)
+     podcast = Podcast(url="Not An URL!",
                                  title="Thomas")
      with pytest.raises(ApiException) as apiexc:
          c.podcasts_create(podcast)
@@ -52,19 +54,19 @@ def test_create_podcast_wrong_url(api_client):
      
     
 def test_delete_podcast(api_client):
-     c = digitalrooster.PodcastsApi(api_client)
+     c = PodcastsApi(api_client)
      c.podcasts_delete("61f6bcda-0334-49a7-bfe9-b206b9f0bef7")
-     r= c.podcasts_read_all()
+     r= c.podcasts_read_all().value
      assert len(r) == 1
       
 def test_delete_podcast_wrong_id(api_client):
-     c = digitalrooster.PodcastsApi(api_client)
+     c = PodcastsApi(api_client)
      with pytest.raises(ApiException) as apiexc:
          c.podcasts_delete("c0ffee00-404c-f776-8619-3c4c2c4da212")
      assert apiexc.value.status == 400
      msg = json.loads(apiexc.value.body)
      assert msg['code'] == 400
      assert msg['message'] == 'no item for this UUID'
-     r= c.podcasts_read_all()
+     r= c.podcasts_read_all().value
      assert len(r) == 2
 
--- test/api-tests/test_radios.py
+++ test/api-tests/test_radios.py
@@ -7,10 +7,12 @@ import pytest
 import digitalrooster
 import json
 from digitalrooster.rest import ApiException
+from digitalrooster.api.radios_api import RadiosApi
+from digitalrooster.model.station import Station
 
 def test_read_stations(api_client):
-    c = digitalrooster.RadiosApi(api_client)
-    r= c.iradio_read_all()
+    c = RadiosApi(api_client)
+    r= c.iradio_read_all().value
     assert len(r) == 2
     assert r[0].id == '0a2152ef-da4f-4f26-8a77-078193da536e'
     assert r[0].name == 'BBC Radio 4'
@@ -18,12 +20,12 @@ def test_read_stations(api_client):
 
 
 def test_read_stations_negative_offset(api_client):
-    c = digitalrooster.RadiosApi(api_client)
-    r= c.iradio_read_all(offset=-1)
+    c = RadiosApi(api_client)
+    r= c.iradio_read_all(offset=-1).value
     assert len(r) == 2
 
 def test_get_station_by_id(api_client):
-    c = digitalrooster.RadiosApi(api_client)
+    c = RadiosApi(api_client)
     r=c.iradio_read_one("09be8e85-a9d3-4db8-b2c5-02e3eb3ff66d")
     assert r.id == "09be8e85-a9d3-4db8-b2c5-02e3eb3ff66d"
     assert r.url == "https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3"
@@ -31,7 +33,7 @@ def test_get_station_by_id(api_client):
 
 
 def test_get_station_by_wrong_id(api_client):
-    c = digitalrooster.RadiosApi(api_client)
+    c = RadiosApi(api_client)
     with pytest.raises(ApiException) as apiexc:
         c.iradio_read_one("c0ffee00-404c-f776-8619-3c4c2c4da212")
     assert apiexc.value.status == 400
@@ -41,16 +43,16 @@ def test_get_station_by_wrong_id(api_client):
 
 
 def test_create_station_ok(api_client):
-     c = digitalrooster.RadiosApi(api_client)
-     station = digitalrooster.Station(name="AName", url="foo://bar.baz")
+     c = RadiosApi(api_client)
+     station = Station(name="AName", url="foo://bar.baz")
      c.iradio_create(station)
-     r= c.iradio_read_all()
+     r= c.iradio_read_all().value
      assert len(r) == 3
 
      
 def test_create_station_wrong_url(api_client):
-     c = digitalrooster.RadiosApi(api_client)
-     station = digitalrooster.Station(url="Not : # üß !an.url/",
+     c = RadiosApi(api_client)
+     station = Station(url="Not : # üß !an.url/",
                                  name="Thomas")
      with pytest.raises(ApiException) as apiexc:
          c.iradio_create(station)
@@ -58,18 +60,18 @@ def test_create_station_wrong_url(api_client):
      
     
 def test_delete_station(api_client):
-     c = digitalrooster.RadiosApi(api_client)
+     c = RadiosApi(api_client)
      c.iradio_delete("09be8e85-a9d3-4db8-b2c5-02e3eb3ff66d")
-     r= c.iradio_read_all()
+     r= c.iradio_read_all().value
      assert len(r) == 1
 
 def test_delete_station_wrong_id(api_client):
-     c = digitalrooster.RadiosApi(api_client)
+     c = RadiosApi(api_client)
      with pytest.raises(ApiException) as apiexc:
          c.iradio_delete("c0ffee00-404c-f776-8619-3c4c2c4da212")
      assert apiexc.value.status == 400
      msg = json.loads(apiexc.value.body)
      assert msg['code'] == 400
      assert msg['message'] == 'no item for this UUID'
-     r= c.iradio_read_all()
+     r= c.iradio_read_all().value
      assert len(r) == 2
