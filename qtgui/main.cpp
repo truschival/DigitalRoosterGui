9c5b393bd63c5faf4f7ac3c2d70e4971b3762734
Thomas Ruschival <thomas@ruschival.de>
2020-12-16 08:44:08 +0100

    fix Signal/Slot connections

--- qtgui/main.cpp
+++ qtgui/main.cpp
@@ -181,8 +181,8 @@ int main(int argc, char* argv[]) {
         &Hal::IHardware::system_poweroff);
 
     /* AlarmDispatcher activates system */
-    QObject::connect(
-        &alarmdispatcher, SIGNAL(alarm_triggered()), &power, SLOT(activate()));
+    QObject::connect(&alarmdispatcher, &AlarmDispatcher::alarm_triggered,
+        &power, &PowerControl::activate);
 
     /* Sleeptimer sends system to standby */
     QObject::connect(&sleeptimer, &SleepTimer::sleep_timer_elapsed, &power,
@@ -191,10 +191,8 @@ int main(int argc, char* argv[]) {
     QObject::connect(&playerproxy, &MediaPlayer::playback_state_changed,
         &sleeptimer, &SleepTimer::playback_state_changed);
     /* Sleeptimer also monitors alarms */
-    QObject::connect(&alarmdispatcher,
-        SIGNAL(alarm_triggered(std::shared_ptr<DigitalRooster::Alarm>)),
-        &sleeptimer,
-        SLOT(alarm_triggered(std::shared_ptr<DigitalRooster::Alarm>)));
+    QObject::connect(&alarmdispatcher, &AlarmDispatcher::alarm_triggered,
+        &sleeptimer, &SleepTimer::alarm_triggered);
 
     /* Rotary encoder push button interface */
     VolumeButton volbtn;
