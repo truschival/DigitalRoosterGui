ffbb099f7b0577de46a65372135182612b7d80ba
Thomas Ruschival <thomas@ruschival.de>
2022-03-06 09:54:25 +0100

    Minor cleanups
    Signed-off-by: Thomas Ruschival <thomas@ruschival.de>

--- include/alarmmonitor.hpp
+++ include/alarmmonitor.hpp
@@ -4,7 +4,7 @@
  *
  * \details
  *
- * \copyright (c) 2018 2018  Thomas Ruschival <thomas@ruschival.de>
+ * \copyright (c) 2018  Thomas Ruschival <thomas@ruschival.de>
  * \license {This file is licensed under GNU PUBLIC LICENSE Version 3 or later
  * 			 SPDX-License-Identifier: GPL-3.0-or-later}
  *
